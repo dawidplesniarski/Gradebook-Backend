dfa9ce5d7435f3d6b3bd03e6eb7d6b72c9c109d7
dawidplesniarski <dawidplesniarski@gmail.com>
2020-04-18 11:12:04 +0200

    init project with structure

---
+++ src/main/java/com/plesniarski/gradebook/GradebookApplication.java
@@ -0,0 +1,13 @@
+package com.plesniarski.gradebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradebookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GradebookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/plesniarski/gradebook/GradebookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plesniarski.gradebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradebookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
