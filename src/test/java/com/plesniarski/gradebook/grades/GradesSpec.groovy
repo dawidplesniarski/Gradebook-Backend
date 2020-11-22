98390356771f94ae1710e81a73639892ebb9480d
dawidplesniarski <dawidplesniarski@gmail.com>
2020-11-22 17:37:53 +0100

    Grades tests added

--- src/test/java/com/plesniarski/gradebook/grades/GradesSpec.groovy
+++ src/test/java/com/plesniarski/gradebook/grades/GradesSpec.groovy
@@ -27,4 +27,24 @@ class GradesSpec extends Specification {
         2.0 | "Kryptografia" | 2
         5.0 | "TIJO" | 3
     }
+
+    def "should find all grades" () {
+        when: "try to find all grades"
+        def response = client.get(path: "grades/findAllGrades");
+        then: "should return list of grades"
+        with (response) {
+            status: 200
+            data.size > 0
+        }
+    }
+
+    def "should return student grades" () {
+        when: "try to find student grades"
+        def response = client.get(path: "grades/findStudentGrades/2");
+        then: "should return list of student grades"
+        with (response) {
+            status: 200
+            data.size > 0
+        }
+    }
 }
